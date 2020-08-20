55ff9369a48a3923944527c685dbbccc1a95deae
André Augusto Giannotti Scotá <andre.scota@gmail.com>
2020-08-20 02:13:01 -0300

    Add files via upload

---
+++ SrvKill.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Andre Augusto Giannotti Scota (https://sites.google.com/view/a2gs/)
+
+# Script exit if a command fails:
+#set -e
+
+# Script exit if a referenced variable is not declared:
+#set -u
+
+# If one command in a pipeline fails, its exit code will be returned as the result of the whole pipeline:
+#set -o pipefail
+
+# Activate tracing:
+#set -x
+
+pidSrvSend=`ps -ef | grep -v grep | grep SrvSend.py | awk '{print $2}'`
+pidSrvDataClient=`ps -ef | grep -v grep | grep SrvDataClient.py | awk '{print $2}'`
+
+[ ! -z "$pidSrvSend" ] && echo "SrvSend PID: [$pidSrvSend]" && kill -9 "$pidSrvSend"
+[ ! -z "$pidSrvDataClient" ] && echo "SrvDataClient PID: [$pidSrvDataClient]" && kill -9 "$pidSrvDataClient"
